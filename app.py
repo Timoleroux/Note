ca6bad3f52f31415e0f4083d1b7a37144def1a14
Timothée Le Roux <timoleroux2006@gmail.com>
2021-07-02 22:25:40 +0200

    10.0

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from PySide2 import QtGui, QtWidgets
 from PySide2.QtCore import QWaitCondition, Qt
 import os
 import json
+import default
 
-PATH = "C:/Users/timol/OneDrive/Documents/GitHub/Note/TEST/dataTEST/dataTEST.json"
+PATH = "C:/Users/timol/OneDrive/Documents/GitHub/Note/data/data.json"
 
 class App(QtWidgets.QWidget):
     def __init__(self):
@@ -12,126 +13,143 @@ class App(QtWidgets.QWidget):
         self.setWindowIcon(QtGui.QIcon('C:/Users/timol/OneDrive/Documents/GitHub/Note/data/icon.ico'))
         self.component()
         self.css()
-        self.note_load()
+        self.load(True)
 
     def component(self):
         self.main_layout = QtWidgets.QHBoxLayout(self)
         self.left_layout = QtWidgets.QVBoxLayout(self)
         self.right_layout = QtWidgets.QVBoxLayout(self)
 
-        self.note_list = QtWidgets.QListWidget()
-        self.open = QtWidgets.QPushButton("Ouvir")
-        self.open.clicked.connect(self.note_open)
-        self.delete = QtWidgets.QPushButton("Supprimer")
-        self.delete.clicked.connect(self.note_remove)
+        self.list_note = QtWidgets.QListWidget()
+        self.list_note.itemClicked.connect(self.note_open)
+        self.btn_delete = QtWidgets.QPushButton("Supprimer")
+        self.btn_delete.clicked.connect(self.note_remove)
 
-        self.title = QtWidgets.QLineEdit()
+        self.le_title = QtWidgets.QLineEdit()
         self.text_area = QtWidgets.QTextEdit()
-        self.save = QtWidgets.QPushButton("Sauvegarder")
-        self.save.clicked.connect(self.note_write)
+        self.text_area.textChanged.connect(self.note_save)
+        self.btn_save = QtWidgets.QPushButton("Nouveau")
+        self.btn_save.clicked.connect(self.note_write)
 
         self.main_layout.addLayout(self.left_layout)
         self.main_layout.addLayout(self.right_layout)
 
-        self.left_layout.addWidget(self.note_list)
-        self.left_layout.addWidget(self.open)
-        self.left_layout.addWidget(self.delete)
-        self.right_layout.addWidget(self.title)
+        self.left_layout.addWidget(self.list_note)
+        self.left_layout.addWidget(self.btn_delete)
+        self.right_layout.addWidget(self.le_title)
         self.right_layout.addWidget(self.text_area)
-        self.right_layout.addWidget(self.save)
+        self.right_layout.addWidget(self.btn_save)
 
     def css(self):
         self.setStyleSheet("""
         background-color: #ffffff;
         """)
 
-    def get_nbr_next_note(self):
-        global nbr_next_note
-        nbr_next_note = 0
-        for i in json_dict:
-            nbr_next_note += 1
-        nbr_next_note = str(nbr_next_note)
-
-    def json_load(self):
+    def load(self, is_load):  # load all elements in text_area at the starting (note_load)
         global json_dict
-        with open(PATH, "r") as f:
-            json_dict = json.load(f)
-            is_empty = bool(json_dict)  # if json_dict is empty return False
-        if not json_dict or is_empty == False:  # if json_dict is empty or don't exist
-            with open(PATH, "w") as f:
-                json.dump({"0":{"title":"How to use this app ?", "content":"It's a simply app of note. All notes are saves in a json file."}}, f, indent=4)
 
         with open(PATH, "r") as f:
             json_dict = json.load(f)
-
-    def json_write(self, title, content):
-        self.json_load()
-        global note_exist
-        note_exist = False
-        for i in json_dict:
-            res = json_dict[i]["title"]
-            if title == res:
-                note_exist = True
-
-        if note_exist == False:
-            json_new = {"title":title, "content":content}
-            self.get_nbr_next_note()
-            json_dict.update({nbr_next_note:json_new})
-            with open(PATH, "w") as f:
-                json.dump(json_dict, f, indent=4)
-
-    def dict_locate(self, target): # get the ID of the note with only the title of the note
-        if target != "":
-            self.json_load()
-            global target_number
-            locate = None
-            target_number = 0
+             
+        if not json_dict or bool(json_dict) == False:  # if there isn't note, create the default note 
+            with open(PATH, "w") as f:                 # and then update the json_dict
+                json.dump(default.note, f, indent=4)
             with open(PATH, "r") as f:
                 json_dict = json.load(f)
-            while locate != target:
-                if str(target_number) in json_dict:
-                    locate = str(json_dict[str(target_number)]["title"])
-                target_number += 1
-            target_number -= 1
 
-    def json_remove(self):
-        self.json_load()
-        self.dict_locate(self.note_list.currentItem().text())
-        del json_dict[str(target_number)]
-        with open(PATH, "w") as f:
-                json.dump(json_dict, f, indent=4)
+        if is_load == True:
 
-    def note_load(self):
-        self.json_load()
-        first_key = list(json_dict.keys())
-        first_key = str(first_key[0])
-        v = int(first_key)
-        for i in json_dict:
-            if str(v) in json_dict:
-                self.note_list.addItem(json_dict[str(v)]["title"])
-            v += 1
+            note_id = list(json_dict.keys())  # get note ID in a list
+            note_id = int(note_id[0])
 
-    def note_write(self):
-            resT = str(self.title.text())
-            resC = str(self.text_area.toPlainText())
-            self.json_write(resT, resC)
-            if note_exist == False:
-                self.note_list.addItem(resT)
-            elif note_exist == True:
-                self.dict_locate(resT)
+            for i in json_dict:  # add all the note in text_area
+                if str(note_id) in json_dict:
+                    self.list_note.addItem(json_dict[str(note_id)]["title"])
+                note_id += 1
 
-    def note_remove(self):
-        self.json_remove()
-        list_items = self.note_list.selectedItems()
-        for item in list_items:
-            self.note_list.takeItem(self.note_list.row(item))
+    def get_id_by_title(self, target_title):
+        global note_id
+
+        if target_title != "":
+
+            self.load(False)
+            goal = None
+            note_id = 0
+
+            while goal != target_title:
+
+                if str(note_id) in json_dict:
+                    goal = str(json_dict[str(note_id)]["title"])
+
+                note_id += 1
+
+        note_id -= 1
+        note_id = str(note_id)
 
     def note_open(self):
-        self.dict_locate(self.note_list.currentItem().text())
-        self.title.setText(json_dict[str(target_number)]["title"])
-        self.text_area.setText(json_dict[str(target_number)]["content"])
+        self.get_id_by_title(self.list_note.currentItem().text())
+        self.le_title.setText(json_dict[note_id]["title"])
+        self.text_area.setText(json_dict[note_id]["content"])
+
+    def note_write(self):
+        self.load(False)
+        
+        def write(title, content):
+
+            is_note_exist = False
+
+            for i in json_dict:  # set if the existing of the note
+                if title == json_dict[i]["title"]:
+                    is_note_exist = True
+
+            if is_note_exist == False and self.le_title.text():  # if the note don't exist and 
+                                                                 # the title isn't empty: create it
+                json_keys_list = list(json_dict.keys()) # get all the ID of the notes
+                id_new_note = json_keys_list[-1]  # get the last ID
+                id_new_note = int(id_new_note)
+                id_new_note += 1  # get the next ID 
+                new_note = {"title":title, "content":content}  # set the new note
+
+                json_dict.update({id_new_note:new_note})  # add the note the json_dict
+
+                with open(PATH, "w") as f:
+                    json.dump(json_dict, f, indent=4)  # add the note in the json file
+                
+                self.list_note.addItem(title) # add the title in the list_note
+            
+            elif is_note_exist == True:
+                self.get_id_by_title(self.le_title.text())
+                new_content = self.text_area.toPlainText()
+                json_dict.update({note_id:{"title":self.le_title.text(), "content":new_content}})
+
+                with open(PATH, "w") as f:
+                    json.dump(json_dict, f, indent=4)
+
+            text_title = str(self.le_title.text())  # get the title for the write function  
+            text_content = str(self.text_area.toPlainText())  # get the content for the write function
+            write(text_title, text_content)
+
+    def note_save(self):
+        self.get_id_by_title(self.le_title.text())
+        new_title = self.le_title.text()
+        new_content = self.text_area.toPlainText()
+        json_dict.update({note_id:{"title":new_title, "content":new_content}})
+
+        with open(PATH, "w") as f:
+            json.dump(json_dict, f, indent=4)
 
 
+    def note_remove(self):
+        self.load(False)
+        self.get_id_by_title(self.list_note.currentItem().text())
+
+        del json_dict[note_id]  # delete the note
+        with open(PATH, "w") as f:
+                json.dump(json_dict, f, indent=4)  # update the json file
+
+        list_items = self.list_note.selectedItems()
+        for item in list_items:
+            self.list_note.takeItem(self.list_note.row(item))  # remove the note title in the list_note
 
 app = QtWidgets.QApplication([])
 win = App()
